ade0c4f4671a539d76f7d1caa4f202d9fd822f9b
luandro <luandro@gmail.com>
2018-05-15 10:23:48 -0300

    added correct patter for nodejs-mobile, deleted nodejs-project from src and left nodejs-assets

--- nodejs-assets/nodejs-project/index.js
+++ nodejs-assets/nodejs-project/index.js
@@ -1,10 +1,8 @@
 const server = require('open-app-graphql-server')
 const ssbDefaults = require('ssb-graphql-defaults')
-const datTypes = require('dat-graphql')
-
-console.log('NodeJS is starting!')
+// const datTypes = require('dat-graphql')
 
 server([
   ssbDefaults,
-  datTypes
+  // datTypes
 ])
