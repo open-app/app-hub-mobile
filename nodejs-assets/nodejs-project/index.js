88c62120ff9997e29e6f5cfff986ed917ae67092
luandro <luandro@gmail.com>
2018-05-15 16:20:18 -0400

    Working with nodejs-mobile

--- nodejs-assets/nodejs-project/index.js
+++ nodejs-assets/nodejs-project/index.js
@@ -2,6 +2,8 @@ const server = require('open-app-graphql-server')
 const ssbDefaults = require('ssb-graphql-defaults')
 // const datTypes = require('dat-graphql')
 
+console.log('Starting GraphQL Server')
+
 server([
   ssbDefaults,
   // datTypes
